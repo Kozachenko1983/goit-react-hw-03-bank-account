7a212c84c07fae012c3f85cbdf54ecdf1ff93d66
Michael Kozachenko <Kozachenko.Michael@gmail.com>
2019-11-03 21:55:18 +0200

    Updates

---
+++ precache-manifest.2eda2861167cf0147d419f99524487e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437adf187679004059ab225fd0d17f38",
+    "url": "/goit-react-hw-03-bank-account/./index.html"
+  },
+  {
+    "revision": "a1fca5d333a4b32ba8f6",
+    "url": "/goit-react-hw-03-bank-account/./static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "4cb57c9b00b08010564e",
+    "url": "/goit-react-hw-03-bank-account/./static/css/main.2237dc33.chunk.css"
+  },
+  {
+    "revision": "a1fca5d333a4b32ba8f6",
+    "url": "/goit-react-hw-03-bank-account/./static/js/2.074fe342.chunk.js"
+  },
+  {
+    "revision": "4cb57c9b00b08010564e",
+    "url": "/goit-react-hw-03-bank-account/./static/js/main.9ca8cceb.chunk.js"
+  },
+  {
+    "revision": "cc619dbc21092f76b779",
+    "url": "/goit-react-hw-03-bank-account/./static/js/runtime-main.3b0cd65b.js"
+  }
+]);
